9f75af8932d54a6a45b57f39b74b7986db74ddc5
David Alexander Leigh <daleighan@gmail.com>
2020-03-03 15:45:41 -0800

    Fixed spelling error

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.set('views', __dirname + '/views');
 
 app.use('/shadows', shadowsRouter);
 
-app.use((err, req, res, next) =>
-  res.status(err.statusCode).json({message: err.message, code: err.code}),
-);
+app.use((err, req, res, next) => {
+  res.status(err.statusCode).json({message: err.message, code: err.code});
+});
 
 app.listen(port, () => console.log(`App listening on ${port}`));
--- telemActions.js
+++ telemActions.js
@@ -2,6 +2,6 @@ module.exports = {
   'lock-unlock': {desired: {can: 'unlock_all'}},
   'lock-lock': {desired: {can: 'lock'}},
   'immobilizer-lock': {desired: {immo: 'lock'}},
-  'immobilizer-unock': {desired: {immo: 'unlock'}},
+  'immobilizer-unlock': {desired: {immo: 'unlock'}},
   'flashLights': {desired: {can: 'flash_lights'}},
 };
