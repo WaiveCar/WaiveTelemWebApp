20b3865253ee6f8385f5e78ddaf7957703465e07
David Alexander Leigh <daleighan@gmail.com>
2020-03-03 15:18:34 -0800

    Renamed can actions

--- telemActions.js
+++ telemActions.js
@@ -1,7 +1,7 @@
 module.exports = {
-  unlock: {desired: {can: 'unlock_all'}},
-  lock: {desired: {can: 'lock'}},
-  immobilize: {desired: {immo: 'lock'}},
-  unimmobilize: {desired: {immo: 'unlock'}},
-  flashLights: {desired: {can: 'flash_lights'}},
+  'lock-unlock': {desired: {can: 'unlock_all'}},
+  'lock-lock': {desired: {can: 'lock'}},
+  'immobilizer-lock': {desired: {immo: 'lock'}},
+  'immobilizer-unock': {desired: {immo: 'unlock'}},
+  'flashLights': {desired: {can: 'flash_lights'}},
 };
